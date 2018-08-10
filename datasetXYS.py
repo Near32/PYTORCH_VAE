074174b699379ac4fcabd2b38080ea2eb080a356
Kevin Denamganai <denamganai.kevin@gmail.com>
2018-08-10 07:00:28 +0100

    added new subtasks loading functions...

--- datasetXYS.py
+++ datasetXYS.py
@@ -193,7 +193,7 @@ def parse_image_annotation_GazeRecognition(ann_dir,img_dir) :
 	imgs = []
 
 	#img_folder = sorted( os.listdir(img_dir) )
-	img_folder = os.listdir(img_dir)
+	img_folder = os.listdir(img_dir)#[:20000]
 	size = len(img_folder)
 	
 	lr = list()
@@ -206,20 +206,29 @@ def parse_image_annotation_GazeRecognition(ann_dir,img_dir) :
 		return img[:-3]+'xml'
 
 	for idx_ann, image in enumerate( img_folder ) :
-		ann = Img2Ann(image)
+		#ann = Img2Ann(image)
+		ann = Img2GazeDirFLAnn(image)
 
 		img = {}
 
-		path2ann = os.path.join(ann_dir,ann)
-		tree = ET.parse(path2ann )
-		
-		print('DATASET :: LOADING : {:0.1f} %'.format( idx_ann/nbrann*100.0), end='\r', flush=True )
+		try :
+			path2ann = os.path.join(ann_dir,ann)
+			tree = ET.parse(path2ann )
+		except Exception as e :
+			try :
+				ann = Img2Ann(image)
+				path2ann = os.path.join(ann_dir,ann)
+				tree = ET.parse(path2ann )
+			except Exception as e2 :
+				print('\n EXCEPTION :: ',e,'\n-- FOLLOWED by :',e2)
+			
+		print('DATASET :: LOADING : {:0.4f} %'.format( idx_ann/nbrann*100.0), end='\r')#, flush=True )
 
 		for elem in tree.iter() :
 			if 'filename' in elem.tag :
 				imgs += [img]
-				img['filename'] = elem.text
-
+				img['filename'] = GazeDirFLFilename2Filename(elem.text)
+				
 			if 'width' in elem.tag :
 				img['width'] = int(float(elem.text))
 			if 'height' in elem.tag :
@@ -304,6 +313,86 @@ def parse_image_annotation_GazeRecognition(ann_dir,img_dir) :
 				
 				if name is not None :
 					img[name] = bndbox
+
+			if 'GazeDirection' in elem.tag:
+				GazeDirection = {'right':None,'left':None}
+				img['GazeDirection'] = GazeDirection
+
+				for attr in list(elem) :
+					if 'right' in attr.tag :
+						right = {}
+						GazeDirection['right'] = right
+						
+						for attri in list(attr) :
+							if 'dir' in attri.tag :
+								direction = {}
+								right['dir'] = direction 
+								
+								for attrii in list(attri) :
+									if 'x' in attrii.tag :
+										direction['x'] = float(attrii.text)
+									if 'y' in attrii.tag :
+										direction['y'] = float(attrii.text)
+									if 'z' in attrii.tag :
+										direction['z'] = float(attrii.text)
+								
+							if 'angle' in attri.tag :
+								angle = {}
+								right['angle'] = angle 
+
+								for attrii in list(attri) :
+									if 'x' in attrii.tag :
+										angle['x'] = float(attrii.text)
+									if 'y' in attrii.tag :
+										angle['y'] = float(attrii.text)
+									if 'z' in attrii.tag :
+										angle['z'] = float(attrii.text)
+								
+
+					if 'left' in attr.tag :
+						left = {}
+						GazeDirection['left'] = left
+						
+						for attri in list(attr) :
+							if 'dir' in attri.tag :
+								direction = {}
+								left['dir'] = direction 
+								
+								for attrii in list(attri) :
+									if 'x' in attrii.tag :
+										direction['x'] = float(attrii.text)
+									if 'y' in attrii.tag :
+										direction['y'] = float(attrii.text)
+									if 'z' in attrii.tag :
+										direction['z'] = float(attrii.text)
+								
+							if 'angle' in attri.tag :
+								angle = {}
+								left['angle'] = angle 
+
+								for attrii in list(attri) :
+									if 'x' in attrii.tag :
+										angle['x'] = float(attrii.text)
+									if 'y' in attrii.tag :
+										angle['y'] = float(attrii.text)
+									if 'z' in attrii.tag :
+										angle['z'] = float(attrii.text)
+								
+
+			if 'FacialLandmarks' in elem.tag:
+				FacialLandmarks = {}
+				img['FacialLandmarks'] = FacialLandmarks
+
+				for attr in list(elem) :
+					fl = {}
+					FacialLandmarks[attr.tag] = fl
+					
+					for attri in list(attr) :
+						if 'x' in attri.tag :
+							fl['x'] = float(attri.text)
+						if 'y' in attri.tag :
+							fl['y'] = float(attri.text)
+						
 				
 	lr = np.asarray(lr)
 	print('ROLL :: mean {} // var {}'.format(lr.mean(),lr.std()))					
@@ -317,6 +406,179 @@ def parse_image_annotation_GazeRecognition(ann_dir,img_dir) :
 
 	return imgs
 
+def Img2Ann(img) :
+	return img[:-3]+'xml'
+def Img2GazeDirFLAnn(img) :
+	return 'GazeDirFacialLandmarks.'+img[:-3]+'xml'
+
+def GazeDirFLFilename2Filename(text) :
+	if len(text) > 23:
+		return text[23:]
+	else :
+		return text 
+
+def parse_image_annotation_GazeRecognition_MT(ann_dir,img_dir) :
+	from threading import Thread, Lock 
+	
+	lock = Lock()
+
+	imgs = []
+	threads = []
+
+	#img_folder = sorted( os.listdir(img_dir) )
+	img_folder = os.listdir(img_dir)
+	size = len(img_folder)
+	
+	lr = list()
+	lp = list()
+	ly = list()
+
+	nbrann = size
+
+	divider = 32
+	q = nbrann // divider 
+	r = nbrann % divider 
+
+
+	listann = []
+	
+	listfile = enumerate( img_folder )
+	
+	for idx_ann, image in listfile :
+		listann.append( (ann_dir,idx_ann,image,imgs,lr,lp,ly,lock ) )
+		print('LAUNCHING THREAD : {} / {}'.format(idx_ann,nbrann), end='\r')
+
+		if idx_ann % divider == 0 :
+			f = lambda x : handleAnn(x)
+			t = Thread(target=f( listann ) )
+			threads.append(t)
+			t.start()
+			listann = []
+
+	for i in range(len(threads)) :
+		print('DATASET :: LOADING : {:0.1f} %'.format( i/len(threads)*100.0), end='\r', flush=True )
+		threads[i].join()			
+				
+	lr = np.asarray(lr)
+	print('ROLL :: mean {} // var {}'.format(lr.mean(),lr.std()))					
+	lp = np.asarray(lp)
+	print('PITCH :: mean {} // var {}'.format(lp.mean(),lp.std()))					
+	ly = np.asarray(ly)
+	print('YAW :: mean {} // var {}'.format(ly.mean(),ly.std()))					
+				
+	#sns.jointplot(x=lr,y=lp,kind='kde')
+	#plt.show()
+
+	return imgs
+
+def handleAnn( listann) :
+	for ann_dir,idx,image,imgs,lr,lp,ly,lock in listann :
+		ann = Img2Ann(image)
+
+		img = {}
+
+		path2ann = os.path.join(ann_dir,ann)
+		tree = ET.parse(path2ann )
+		
+		
+		for elem in tree.iter() :
+			if 'filename' in elem.tag :
+				lock.acquire()
+				imgs += [img]
+				lock.release()
+
+				img['filename'] = elem.text
+
+			if 'width' in elem.tag :
+				img['width'] = int(float(elem.text))
+			if 'height' in elem.tag :
+				img['height'] = int(float(elem.text))
+			
+			if 'data' in elem.tag:
+				data = {}
+				img['data'] = data
+				
+				for attr in list(elem) :
+					if 'model' in attr.tag :
+						data['model'] = attr.text
+					if 'gaze_position' in attr.tag :
+						gaze = {}
+						data['gaze'] = gaze
+						
+						for attri in list(attr) :
+							if 'x' in attri.tag :
+								gaze['x'] = float(attri.text)
+							if 'y' in attri.tag :
+								gaze['y'] = float(attri.text)
+					if 'screen_size' in attr.tag :
+						screen = {}
+						data['screen'] = screen
+						
+						for attri in list(attr) :
+							if 'width' in attri.tag :
+								screen['width'] = float(attri.text)
+							if 'height' in attri.tag :
+								screen['height'] = float(attri.text)
+					if 'camera_screen' in attr.tag :
+						cam_screen = {}
+						data['camera_screen_center_offset'] = cam_screen
+						
+						for attri in list(attr) :
+							if 'x' in attri.tag :
+								cam_screen['x'] = float(attri.text)
+							if 'y' in attri.tag :
+								cam_screen['y'] = float(attri.text)
+							if 'fov' in attri.tag :
+								cam_screen['fov'] = float(attri.text)
+
+					if 'head' in attr.tag :
+						head = {}
+						data['head'] = head
+
+						for attri in list(attr) :
+							if 'head_camera_distance' in attri.tag :
+								head['head_camera_distance'] = float(attri.text)
+							if 'head_camera_distanceX' in attri.tag :
+								head['head_camera_distanceX'] = float(attri.text)
+							if 'head_camera_distanceY' in attri.tag :
+								head['head_camera_distanceY'] = float(attri.text)
+							if 'head_roll' in attri.tag :
+								head['head_roll'] = float(attri.text)
+								lock.acquire()
+								lr.append(head['head_roll']*180/pi)
+								lock.release()
+							if 'head_pitch' in attri.tag :
+								head['head_pitch'] = float(attri.text)
+								lock.acquire()
+								lp.append(head['head_pitch']*180/pi)
+								lock.release()
+							if 'head_yaw' in attri.tag :
+								head['head_yaw'] = float(attri.text)
+								lock.acquire()
+								ly.append(head['head_yaw']*180/pi)
+								lock.release()
+						
+
+			if 'object' in elem.tag:
+				name = None
+				bndbox = [0,0,0,0]
+
+				for attr in list(elem) :
+					if 'name' in attr.tag :
+						name = attr.text
+					if 'bndbox' in attr.tag :
+						for attri in list(attr) :
+							if 'xmin' in attri.tag :
+								bndbox[0] = float(attri.text)
+							if 'ymin' in attri.tag :
+								bndbox[1] = float(attri.text)
+							if 'xmax' in attri.tag :
+								bndbox[2] = float(attri.text)
+							if 'ymax' in attri.tag :
+								bndbox[3] = float(attri.text)
+				
+				if name is not None :
+					img[name] = bndbox
 
 def parse_annotation_GazeRecognition(ann_dir) :
 	imgs = []
@@ -449,6 +711,8 @@ class DatasetGazeRecognition(Dataset) :
 		print('iTrackerNOGRID Format : ',self.iTrackerNoGridFormat)
 
 		self.default_head_pose = torch.zeros((6))
+		self.default_facialLandmarks = torch.zeros((16,2))
+		self.default_gaze_directions = torch.zeros((3*2))
 
 		self.testing = False
 		#self.nbrTestModels = 4
@@ -483,6 +747,9 @@ class DatasetGazeRecognition(Dataset) :
 		self.testoffset = len(self.parsedAnnotations)-self.testsize
 		print('TESTING SIZE : {}'.format(self.testsize))
 
+		self.filenameSortedList = [ (idx, el['filename']) for idx,el in enumerate(self.parsedAnnotations)]
+		self.filenameSortedList = sorted(self.filenameSortedList, key= lambda x:x[1])
+		
 	def __len__(self) :
 		if not(self.testing):
 			ret = len(self.parsedAnnotations)-self.testsize
@@ -490,8 +757,8 @@ class DatasetGazeRecognition(Dataset) :
 				ret = self.testsize
 			return ret 
 		else :
-			#return self.testsize
-			return int(self.testsize/10)
+			return self.testsize
+			#return int(self.testsize/10)
 		'''
 		return len(self.parsedAnnotations)
 		'''
@@ -600,31 +867,78 @@ class DatasetGazeRecognition(Dataset) :
 
 	def __getitem__(self,idx) :
 		if self.testing :
-			idx = idx % self.testsize
-			idx = idx + self.testoffset
+			idx = idx % len(self)
+			model_idx = -1
+			model_name = self.idx2model[model_idx]
+			nbrSample4Model = self.nbrSample4Task(model_idx)
+			psidx = 0 
+			samplingidx = psidx + nbrSample4Model 
+			while idx >= samplingidx:
+				model_idx -= 1
+				model_name = self.idx2model[model_idx]
+				nbrSample4Model = self.nbrSample4Task(model_idx)
+				psidx = samplingidx
+				samplingidx += nbrSample4Model 
+			
+			samplingidx = idx - psidx 	
+			idx = self.idxModels[model_name][samplingidx]
 		else :
 			idx = idx % len(self)
+			
 			model_idx = 0
 			model_name = self.idx2model[model_idx]
-			while idx >= len(self.idxModels[model_name]) :
-				idx -= len(self.idxModels[model_name])
-				model_idx +=1
+			nbrSample4Model = self.nbrSample4Task(model_idx)
+			psidx = 0 
+			samplingidx = psidx + nbrSample4Model 
+			while idx >= samplingidx :
+				model_idx += 1
 				model_name = self.idx2model[model_idx]
-
-
+				nbrSample4Model = self.nbrSample4Task(model_idx)
+				psidx = samplingidx
+				samplingidx += nbrSample4Model 
+			
+			samplingidx = idx - psidx 	
+			try :
+				idx = self.idxModels[model_name][samplingidx]
+			except Exception as e:
+				print(e)
+				print(idx,model_name,samplingidx)
+				print(len(self.idxModels[model_name]))
+				raise e 
+			'''
+			idx = self.filenameSortedList[idx][0]
+			'''
+			
 		issue = True
 		while issue :
 			try :
 				path = os.path.join(self.img_dir,self.parsedAnnotations[idx]['filename']+'.png' )
+				#print(path)
 				img = cv2.imread(path)
-				denoising_img = randomizeHSV(img)
+				if self.denoising :
+					denoising_img = randomizeHSV(img)
 				img = randomizeBackground(img,black=True)
 				h,w,c = img.shape 
 				issue = False
 			except Exception as e :
-				print(e,idx)
+				print(e,idx,path)
 				idx = (idx+1)%len(self)
-					
+
+		#print('IDX : ',idx)
+		#print('Model : ', self.parsedAnnotations[idx]['data']['model'])
+		
+		# FacialLandmarks :
+		scalarfl = {'x':w,'y':h}
+		try :
+			facialLandmarks = copy.deepcopy( self.parsedAnnotations[idx]['FacialLandmarks'])
+			fls = []
+			for k in facialLandmarks.keys() :
+				fli = facialLandmarks[k]
+				fls.append( torch.FloatTensor([ fli[dim]/scalarfl[dim] for dim in sorted(fli.keys() ) ]).view((1,2)) )
+			facialLandmarks = torch.cat( fls, dim=0).view((-1))
+		except Exception as e :
+			print('EXCEPTION LOADING FACIAL LANDMARKS :',e)
+			facialLandmarks = self.default_facialLandmarks
 
 		if self.stacking :
 			img = img#np.expand_dims( cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 2)
@@ -648,9 +962,9 @@ class DatasetGazeRecognition(Dataset) :
 			fx2 = int( min( max(0,face_bndbox[2]/scalar), w) )
 			
 			face_img = img[fy1:fy2, fx1:fx2,:]
-			# Random cropping over the eyes :
+			# Random cropping over the face :
 			if self.randomcropping:
-				off = random.randint(32,96)
+				off = random.randint(1,128)
 				ow, oh = w+off,h+off			
 				face_img = cv2.resize(face_img, (ow,oh))
 				top = np.random.randint(0, oh-h)
@@ -673,7 +987,7 @@ class DatasetGazeRecognition(Dataset) :
 			reye_img = img[ry1:ry2, rx1:rx2,:]
 			# Random cropping over the eyes :
 			if self.randomcropping:
-				off = random.randint(32,96)
+				off = random.randint(1,128)
 				ow, oh = w+off,h+off			
 				reye_img = cv2.resize(reye_img, (ow,oh))
 				top = np.random.randint(0, oh-h)
@@ -691,7 +1005,7 @@ class DatasetGazeRecognition(Dataset) :
 			leye_img = img[ly1:ly2, lx1:lx2,:]
 			# Random cropping over the eyes :
 			if self.randomcropping:
-				off = random.randint(32,96)
+				off = random.randint(1,128)
 				ow, oh = w+off,h+off			
 				leye_img = cv2.resize(leye_img, (ow,oh))
 				top = np.random.randint(0, oh-h)
@@ -768,6 +1082,19 @@ class DatasetGazeRecognition(Dataset) :
 			print(e)
 			head_pose = self.default_head_pose	
 
+		# GazeDirection :
+		try :
+			gaze_direction_r = copy.deepcopy( self.parsedAnnotations[idx]['GazeDirection']['right'])
+			gaze_direction_r = gaze_direction_r['dir']
+			gaze_direction_r = torch.FloatTensor([ gaze_direction_r[k] for k in sorted(gaze_direction_r.keys() ) ]).view((-1))
+			gaze_direction_l = copy.deepcopy( self.parsedAnnotations[idx]['GazeDirection']['left'])
+			gaze_direction_l = gaze_direction_l['dir']
+			gaze_direction_l = torch.FloatTensor([ gaze_direction_l[k] for k in sorted(gaze_direction_l.keys() ) ]).view((-1))
+			gaze_directions = torch.cat( [gaze_direction_r,gaze_direction_l],dim=0)
+		except Exception as e :
+			print('EXCEPTION LOADING GAZE DIRECTIONS :',e)
+			gaze_directions = self.default_gaze_directions	
+
 		try :
 			fov = cam_screen_offset['fov']
 		except Exception as e :
@@ -793,6 +1120,8 @@ class DatasetGazeRecognition(Dataset) :
 			sample.update( {'fov': torch.ones((1,1))*fov })
 
 		sample.update( {'head_pose':head_pose})
+		sample.update( {'facialLandmarks':facialLandmarks})
+		sample.update( {'gazeDirections':gaze_directions})
 
 		return sample
 
@@ -1052,7 +1381,8 @@ def test_stacking() :
 def test_noising() :
 	from time import time 
 	#dataset = load_dataset_XYSM2_C3D_EF(stacking=True)
-	dataset = load_dataset_XYSM1(img_dim=224,stacking=True,randomcropping=True,denoising=True)
+	dataset = load_datasetX6Y3S5M6_121517182122_Roll4Pitch4_40deg_CX5_30cm_CY5_30cm(img_dim=224,stacking=True,randomcropping=True,denoising=True)
+	dataset.testing = True 
 
 	idx = 0 
 	sample = dataset[0]
@@ -1322,6 +1652,35 @@ def load_datasetXYSM2_Roll5Pitch10_60deg_CEl4F_G10X5Y(img_dim=224,stacking=False
 	
 	return datasets
 
+
+def load_datasetXYSM4_0389_Roll10Pitch10_60deg(img_dim=224,stacking=False,randomcropping=False,denoising=False,iTrackerFormat=False,iTrackerNoGridFormat=False) :
+	ann_dir = './dataset-XYSM4-0389-Roll10Pitch10+60deg/annotations'
+	img_dir = './dataset-XYSM4-0389-Roll10Pitch10+60deg/images'
+	width = img_dim
+	height = img_dim
+	transform = Transform 
+	#transform = TransformPlus
+
+	datasets = DatasetGazeRecognition(img_dir=img_dir,ann_dir=ann_dir,width=width,height=height,transform=transform, stacking=stacking, divide2=False,randomcropping=randomcropping,denoising=denoising,iTrackerFormat=iTrackerFormat,iTrackerNoGridFormat=iTrackerNoGridFormat)
+	
+	return datasets
+
+
+def load_datasetX6Y3S5M6_121517182122_Roll4Pitch4_40deg_CX5_30cm_CY5_30cm(img_dim=224,stacking=False,randomcropping=False,denoising=False,iTrackerFormat=False,iTrackerNoGridFormat=False) :
+	ann_dir = './dataset121517182122/annotations'
+	img_dir = './dataset121517182122/images'
+	#ann_dir = './dataset-X6Y3S5M6-121517182122-Roll4Pitch4+40deg-CX5+30cm-CY5+30cm/annotations'
+	#img_dir = './dataset-X6Y3S5M6-121517182122-Roll4Pitch4+40deg-CX5+30cm-CY5+30cm/images'
+	width = img_dim
+	height = img_dim
+	transform = Transform 
+	#transform = TransformPlus
+
+	datasets = DatasetGazeRecognition(img_dir=img_dir,ann_dir=ann_dir,width=width,height=height,transform=transform, stacking=stacking, divide2=False,randomcropping=randomcropping,denoising=denoising,iTrackerFormat=iTrackerFormat,iTrackerNoGridFormat=iTrackerNoGridFormat)
+	
+	return datasets
+
+
 def generateIDX(dataset) :
 	from math import floor
 	nbrel = len(dataset.parsedAnnotations)
@@ -1389,11 +1748,67 @@ def test() :
 	print( len(idxhead) )
 
 
+def draw_landmarks(image,landmarks,n=-1) :
+	nbrLandmarks = landmarks.size(0)
+	h,w,c = image.shape
+	for i in range(nbrLandmarks) :
+		if i == n :
+			color = (0,0,255)
+		else :
+			color = (0,255,0)
+		pos = (int(h*landmarks[i,0]),int(w*landmarks[i,1]) )
+		cv2.circle(image, pos, 2, color, -1)
+	return image 
+
+def test_facialLandmarks() :
+	dataset = load_datasetX6Y3S5M6_121517182122_Roll4Pitch4_40deg_CX5_30cm_CY5_30cm(img_dim=224,stacking=True,randomcropping=False,denoising=False,iTrackerFormat=False,iTrackerNoGridFormat=False)
+	dataset.testing = True 
+
+	i = 0
+	n = 0 
+	continuer = True
+	while continuer :
+		#i = i % len(dataset)
+
+		sample = dataset[i]
+
+		#print(i, sample['image'].size(), sample['landmarks'])
+
+		img = sample['image'].numpy().transpose( (1,2,0))
+		img = img[:,:,0:3]
+		img = np.ascontiguousarray(img)
+		#print(img.shape)
+		#print('Sample size :',sample['facialLandmarks'].size())
+		try :
+			img = draw_landmarks(img, sample['facialLandmarks'],n=n)
+		except Exception as e :
+			print(e)
+
+		cv2.imshow('test', img )
+		print('Image :',i,' // Point : ',n)
+
+		while True :
+			key = cv2.waitKey(30)
+			if key == ord('q'):
+				continuer = False
+				break
+			if key == ord('n') :
+				n += 1
+				break
+			if key == ord('p') :
+				n -= 1
+				break
+			if key == ord('i') :
+				i += 1
+				break
+
+
 if __name__ == '__main__' :
 	#test_dataset()
 	#test_dataset_visualization()
 	#test_error_visualization()
 	#test_stacking()
-	test_noising()
+	#test_noising()
 	#test_iTrackerFormat()
 	#test()
+	test_facialLandmarks()
